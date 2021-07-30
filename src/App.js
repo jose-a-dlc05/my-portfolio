173ef6a92995c2d12c57a599cb192b190cf6c910
Jose De La Cruz <jose.delcruz0490@icloud.com>
2021-07-30 01:23:37 -0400

    Added about component with image

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Navbar from "./components/Navbar.component";
+import About from "./components/About.component";
+import Projects from "./components/Projects.component";
+import Skills from "./components/Skills.component";
+import Contact from "./components/Contact.component";
 
 export default function App() {
 	return (
-		<main>
+		<main className='text-gray-400 bg-gray-900 body-font'>
 			<Navbar />
 			<About />
 			<Projects />
